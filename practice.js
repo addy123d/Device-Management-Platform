2e20502f6b7cca36f3f4a6332ad7fa9dd346ae4c
unknown <acd3101282@gmail.com>
2020-10-29 15:53:34 +0530

    v3.0

--- practice.js
+++ practice.js
@@ -1,7 +1,36 @@
 //This file is just for practice purpose !
 //Create object
-const arr = [1,2,3,4,5];
+// const arr = [1,2,3,4,5];
 
-for(var i = 0 ; i < arr.length; i++){
-    console.log(arr[i])
-}
+// for(var i = 0 ; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+//Generate ID
+// const id = Math.random();
+
+// console.log("Number :",id);
+
+//String Conversion
+// console.log("String :",id.toString());
+
+//After splitting at .
+// console.log("Array :",id.toString().split("."));
+
+//Final string
+// console.log("Final ID :",id.toString().split(".")[1]);
+
+
+//Generate API KEY
+// const api_key = Math.random();
+
+// console.log("API :",api_key);
+// const apiNumber = api_key;
+
+//STRING CONVERSION
+// const apiString = apiNumber.toString(16);
+// console.log("API STR :",apiString.split(".")[1]);
+
+//Generate Time
+const time = new Date().toLocaleString().split(",")[0];
+console.log(time);
--- server.js
+++ server.js
@@ -40,32 +40,108 @@ app.post("/registerDetails",function(request,response){
     //[x]. PUSH user object into an users array
 
     //Check if user exists or not !
-    const getIndex = users.findIndex((user) => user.email === email);
-    console.log(getIndex);
+
+    // var userExists = false;
+
+    // for (var i = 0 ; i< users.length ; i++){
+    //     if(users[i].email === email){
+    //         userExists = true;
+    //         break;
+    //     };
+    // };
+
+    // if(userExists === true){
+    //     response.send("Error : User Exists Already !❌");
+    // }else{
+    //     //Create User object👤
+    //     let user = {
+    //         email : email,
+    //         password : password
+    //     }
+
+    //     //Push user object into users array !
+    //     users.push(user);
+    //     console.log(users);
+
+    //     //Send Registration successful message✅ !
+    //     response.send("registration successful !🎉");
+
+    // };
+
+     const getIndex = users.findIndex((user) => user.email === email);
+     console.log(getIndex);
     
-    if(getIndex < 0){
+     if(getIndex < 0){
 
-        //Create User object👤
-        let user = {
-            email : email,
-            password : password
-        }
+        //const,var,let
+        let today = new Date().toLocaleString().split(",")[0];
 
-        //Push user object into users array !
+
+        //  Create User object👤
+         let user = {
+             _id : Math.random().toString().split(".")[1],
+             email : email,
+             password : password,
+             api_key : Math.random().toString(16).split(".")[1],
+             usage :[{
+                 date : today,
+                 count : 0
+             }]
+         }
+
+        //  Push user object into users array !
         users.push(user);
         console.log(users);
 
-        //Send Registration successful message✅ !
+        //  Send Registration successful message✅ !
         response.send("registration successful !🎉");
-    }else{
-        
-        //Send Error message❌ !
-        response.send("Error !");
-    }
 
-    // TODO - loop problem is remaining !😫
+     }else{
+        
+        //  Send Error message❌ !
+          response.send("Error !");
+     }
 
 });
+
+// Login
+app.get("/login",function(request,response){
+    response.send(`<form action="/loginDetails" method="POST">
+                    <input type="email" name="email" placeholder="Email📧" autocomplete="off">
+                    <input type="password" name="password" placeholder="Choose Password" autocomplete="off">
+                    <button>Submit</button>
+                 `);
+});
+
+app.post("/loginDetails",function(request,response){
+    const email = request.body.email;
+    const password = request.body.password;
+
+    console.log("Email :",email);
+    console.log("Password :",password);
+
+
+    //Login logic
+    //x[]Search users array and check whether user exists or not !
+    //x[] If user doesn't exists then send error message
+
+    const getIndex = users.findIndex((user)=>user.email === email);
+    console.log("Index :",getIndex);
+
+    if(getIndex < 0){
+        response.send("Error : User doesn't exists !😥");
+    }else{
+        //User exists !
+        //Password matching
+        if(users[getIndex].password === password){
+            response.send("Success : Logged In !🎉");
+        }else{
+            response.send("Error :Password Incorrect !❌");
+        }
+    }
+
+})
+
 
 
 app.listen(port,host,function(){
