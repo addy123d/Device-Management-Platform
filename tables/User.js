fb19da617b4f2e68780933b5d2c8b690a600a04b
unknown <acd3101282@gmail.com>
2020-11-10 17:35:40 +0530

    v13.0

---
+++ tables/User.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+    email : {
+        type: String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    plan : {
+        type : String,
+        required : true
+    },
+    Freeplan_usage :{
+        type : String,
+        required : true
+    },
+    api_key :{
+        type : String,
+        required : true
+    },
+    usage :[
+        {
+            date :{
+                type : String,
+                required: true
+            },
+            count :{
+                type : Number,
+                required :  true
+            }
+        }
+    ],
+    projectTitle :{
+        type : Array,
+        required :  true
+    },
+    ips :{
+        type : Array,
+        required :  true
+    },
+    pinNumber :{
+        type : Array,
+        required :  true
+    },
+    projectDescription :{
+        type : Array,
+        required :  true
+    }
+});
+
+module.exports = User = mongoose.model("myUser",userSchema);
